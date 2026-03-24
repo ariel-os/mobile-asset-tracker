7156b58e40156cee422d3045b54064be200bbf34
Nils Ponsard <nils.ponsard@inria.fr>
2026-03-24 14:41:56 +0100

    feat: sequentially get GNSS fix then do networking

--- gateway/src/sensors.rs
+++ gateway/src/sensors.rs
@@ -3,7 +3,6 @@ pub static NRF91_GNSS: ariel_os_nrf91_gnss::Nrf91Gnss =
 #[ariel_os::reexports::linkme::distributed_slice(ariel_os::sensors::SENSOR_REFS)]
 static NRF91_GNSS_REF: &'static dyn ariel_os::sensors::Sensor = &NRF91_GNSS;
 
-#[ariel_os::task]
 pub async fn nrf91_gnss_runner() {
     NRF91_GNSS.run().await
 }
